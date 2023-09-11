05f4cc1dd870fe6cd750f27acc07821e7d79fc9b
Dennisse001 <88120057+Dennisse001@users.noreply.github.com>
2023-09-11 11:11:34 +0700

    Update CategoryController.php

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    public function category()  {
-        return view('categories');
+        public function category()  {
+            return view('categories');
 }
 }
